7222622ecbb487fb51dcf31959ed6f8def5cec9b
Maxim Makarov <mistermaks97@mail.ru>
2024-05-29 03:52:58 +0300

    Fixes

--- cmd/shortener/config.go
+++ cmd/shortener/config.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"flag"
 	"net/url"
+	"os"
 	"strings"
 
 	"github.com/caarlos0/env/v11"
@@ -23,22 +24,31 @@ type Config struct {
 	// Пример: http://localhost:8080/blablabla
 	BaseURL         string `env:"BASE_URL"`
 	LogLevel        string `env:"LOG_LEVEL"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"FileStoragePath"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH"`
 }
 
 func (c *Config) parseFlags() error {
 	flag.StringVar(&c.ServerAddress, "a", "", "Server address")
 	flag.StringVar(&c.BaseURL, "b", "", "Base URL")
 	flag.StringVar(&c.LogLevel, "l", "", "Log level")
-	flag.StringVar(&c.FileStoragePath, "f", FileStoragePath, "File storage path")
+	flag.StringVar(&c.FileStoragePath, "f", "", "File storage path")
 	flag.Parse()
+
+	foundFlagFileStoragePath := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == "f" {
+			foundFlagFileStoragePath = true
+		}
+	})
+
+	_, foundEnvFileStoragePath := os.LookupEnv("FILE_STORAGE_PATH")
 
 	err := env.Parse(c)
 	if err != nil {
 		return err
 	}
 
-	// Если не ввели -a, -b, -l то значения по-умолчанию
+	// Если не ввели -a, -b, -l, -f то значения по-умолчанию
 	if c.ServerAddress == "" {
 		c.ServerAddress = Addr
 	}
@@ -47,6 +57,9 @@ func (c *Config) parseFlags() error {
 	}
 	if c.LogLevel == "" {
 		c.LogLevel = LogLevel
+	}
+	if !foundFlagFileStoragePath && !foundEnvFileStoragePath {
+		c.FileStoragePath = FileStoragePath
 	}
 
 	_, err = url.ParseRequestURI(c.BaseURL)
